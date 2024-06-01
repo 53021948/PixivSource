d38f2c166b13e4c8c567dcb80fbce4c0cdedece1
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2024-06-02 00:51:32 +0800

    替换 Pixiv 分页符标记符号 [newpage]

--- pixiv/content.js
+++ pixiv/content.js
@@ -14,10 +14,11 @@ function objParse(obj) {
 (() => {
     let res = JSON.parse(result).body
     let content = res.content
-    // 正文内部加入小说描述
+    // 在正文内部添加小说描述
     if (res.seriesNavData !== undefined && res.seriesNavData !== null && res.description !== "") {
         content = res.description + "\n" + "——————————\n".repeat(2) + content
     }
+
     // 获取 [uploadedimage:] 的图片链接
     let hasEmbeddedImages = res.textEmbeddedImages !== undefined && res.textEmbeddedImages !== null
     if (hasEmbeddedImages) {
@@ -41,8 +42,30 @@ function objParse(obj) {
         }
     }
 
-    // 替换 Pixiv 其他标记符号 `[[jumpuri: > ]]`
+
+    // 替换 Pixiv 分页符标记符号 `[newpage]`
     content = content.replace(`[newpage]`, `<div style="page-break-after:always"></div>`)
+
+    // 替换 Pixiv 章节标记符号 `[chapter:]`
+    rex = /\[chapter:(.*)]/gm
+    matched = content.match(RegExp(rex))
+    if (matched) {
+        for (let i in matched) {
+            let chapter = matched[i].replace("[chapter:", "").replace("]", "").trim()
+            if (("第".includes(chapter))||("章".includes(chapter))){
+                // chapter = `${chapter}`
+            }
+            else {
+                chapter = `第${Number(i)+1}节 ${chapter}`
+            }
+            content = content.replace(`${matched[i]}`, `${"<p>​<p/>".repeat(3)}${chapter}<p>​<p/>`)
+        }
+    }
+
+
+    // 替换 Pixiv 其他标记符号 `[jump: > ]]`
     // content = content.replace(`[[jumpuri: > ]]`, `<a href="https://www.w3school.com.cn">访问 w3school.com.cn!</a>`)
+
+
     return content
 })()
