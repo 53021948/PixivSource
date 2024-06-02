86c25d63ac9ecce5819684ff9a7d25d1076c61c8
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2024-06-02 18:19:00 +0800

    替换 Pixiv 链接标记符号 `[[jumpuri: > ]]`

--- pixiv/content.js
+++ pixiv/content.js
@@ -43,15 +43,14 @@ function objParse(obj) {
     }
 
 
-    // 替换 Pixiv 分页符标记符号 `[newpage]`
+    // 替换 Pixiv 标记符号 [newpage] [chapter:]
+    // 阅读不支持分页符，因此在 `[chapter:]`添加空行
     content = content.replace(`[newpage]`, `<div style="page-break-after:always"></div>`)
-
-    // 替换 Pixiv 章节标记符号 `[chapter:]`
-    rex = /\[chapter:(.*)]/gm
-    matched = content.match(RegExp(rex))
+    matched = content.match(RegExp(/\[chapter:(.*)]/gm))
     if (matched) {
         for (let i in matched) {
-            let chapter = matched[i].replace("[chapter:", "").replace("]", "").trim()
+            let matched2 = matched[i].match(/\[chapter:(.*)]/m)
+            let chapter = matched2[1].trim()
             if (("第".includes(chapter))||("章".includes(chapter))){
                 // chapter = `${chapter}`
             }
@@ -63,9 +62,24 @@ function objParse(obj) {
     }
 
 
-    // 替换 Pixiv 其他标记符号 `[jump: > ]]`
-    // content = content.replace(`[[jumpuri: > ]]`, `<a href="https://www.w3school.com.cn">访问 w3school.com.cn!</a>`)
-
+    // 替换 Pixiv 链接标记符号 `[[jumpuri: > ]]`
+    matched = content.match(/\[\[jumpuri:(.*)>(.*)?]?]/gm)
+    if (matched) {
+        for (let i in matched) {
+            let matched2 = matched[i].match(/\[\[jumpuri:(.*)>(.*)]]/m)
+            let matchedText = matched2[0]
+            let urlName = matched2[1].trim()
+            let urlLink = matched2[2].trim()
+            // 阅读不支持超链接
+            //content = content.replace(`${matchedText}`, `<a href=${urlLink}>${urlName}</a>`)
+            if (urlLink === urlName) {
+                content = content.replace(`${matchedText}`, `${urlName}: ${urlLink}`)
+            }
+            else{
+                content = content.replace(`${matchedText}`, `${urlName}`)
+            }
+        }
+    }
 
     return content
 })()
