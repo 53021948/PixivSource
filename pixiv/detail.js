6c77e1c86079ccc9d4115901b4f73517acfb411a
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2024-06-03 21:59:59 +0800

    添加链接：无法获取书名作者等信息

--- pixiv/detail.js
+++ pixiv/detail.js
@@ -12,15 +12,17 @@ function objParse(obj) {
 
 
 (function (res) {
-    let isHtml = res.startsWith("<!DOCTYPE html>")
+    let isHtml = result.startsWith("<!DOCTYPE html>")
+    java.log(isHtml)
     if (isHtml) {
         let matchResult = baseUrl.match(new RegExp("pixiv.net/novel"))
         if (matchResult == null) {
             return []
         }
         let id = baseUrl.match(new RegExp("\\d+"))[0]
-        res = JSON.parse(java.ajax(util.urlNovelDetailed(id)))
-
+        java.log(id)
+        res = JSON.parse(java.ajax(util.urlNovelDetailed(id)))["body"]
+        return res
     } else {
         res = JSON.parse(res)
         if (res.total === 0) {
@@ -28,29 +30,17 @@ function objParse(obj) {
         }
     }
 
-
-    java.log(`详情信息:${result}`)
-    res = JSON.parse(result)
-    info = {}
+    let info = {}
     info.author = res.userName
-    // info.name = res.title
+    info.name = res.title
     // info.tags = res.tags.tags
     info.wordCount = res.wordCount
     info.latestChapter = null
     info.desc = res.description
     info.coverUrl = res.coverUrl
-    info.catalogUrl = util.urlNovelDetailed(res.id)
+    info.catalogUrl = util.urlNovelDetailed(res.body.id)
 
-    // info.author = book.author
-    // info.name = book.name
-    // info.tags = book.kind
-    // info.wordCount = book.wordCount
-    // info.latestChapter = null
-    // info.desc = book.intro
-    // info.coverUrl = book.coverUrl
-    // info.catalogUrl = util.urlNovelDetailed(res.id)
-
-    if (res.series === undefined || res.series === null) {
+    if (res.seriesNavData === undefined || res.seriesNavData === null) {
         info.name = res.title
         info.catalog = util.urlNovelDetailed(res.id)
         res.tags.unshift('单本')
@@ -58,9 +48,8 @@ function objParse(obj) {
         info.name = res.seriesNavData.title
         res.tags.unshift('长篇')
         info.catalog = util.urlSeries(res.seriesNavData.id)
-            // `https://www.pixiv.net/ajax/novel/series/${res.seriesNavData.id}?lang=zh`
     }
-    info['classes'] = res.tags.join(",")
+    // info['classes'] = res.tags.join(",")
 
     return info
 })();
